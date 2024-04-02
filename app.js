b6ad7760492f7f8b3a56bda0785985fec91d6efc
Jens Moll-Elsborg <jens1994@gmail.com>
2024-04-02 14:14:52 +0200

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dude!'));
+app.get('/', (req, res) => res.send('Hello my little friend!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
